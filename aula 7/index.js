51f2a9565a3ed901e249f8917d3ecaaa78e4521f
Ricardo Ribeiro <ricardoribeirodematos@gmail.com>
2024-08-25 21:00:33 -0300

    adicionando uma nova branch

---
+++ aula 7/index.js
@@ -0,0 +1,19 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Sempre usar o camelCase
+// Não podem conter espaços ou traços
+// Case-sensitive (Letra maiscúla e letra minuscúla faz diferença)
+// Não pode modificar o valor de uma constante
+// Não utilize var ou LET e sim Const
+
+// + - * / ->> Operadores matemáticos
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(primeiroNumero + segundoNumero);
