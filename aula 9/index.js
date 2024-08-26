cc6346fb7e3e3fb0c818155b43d0fc6386005de4
Ricardo Ribeiro <ricardoribeirodematos@gmail.com>
2024-08-25 21:03:37 -0300

    usando uma nova branch

---
+++ aula 9/index.js
@@ -0,0 +1,7 @@
+nome = "Ricardo"; // Não faça isso, pois gera uma variavel global
+console.log(nome);
+
+//Para redeclarar o Let
+
+let number = 500;
+number = 600; // sem escrever Let novamente, pois irá dar erro, diferente do "Var" que libera isso, mas foi mudado com (ES6)
